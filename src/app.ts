6ef3f0d7527c43d5511ea7a5b0926b8844cd9c93
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-09-21 12:52:47 +0200

    style: destructure currencies class

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import { Currencies } from "./currency.js";
 import { Money } from "./money.js";
 
-const money2 = new Money("7", Currencies.EUR());
-const money3 = new Money("6", Currencies.EUR());
-const money4 = new Money("8", Currencies.EUR());
+const { EUR } = Currencies;
+
+const money2 = new Money("7", EUR());
+const money3 = new Money("6", EUR());
+const money4 = new Money("8", EUR());
 
 console.log(money2.gt(money3));
 console.log(money2.gte(money3));
