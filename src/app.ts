b6fb4f21c73e78e42af8b15cf064b4d60d5429aa
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-09-21 12:54:08 +0200

    refactor: example showcase different input types

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import { Money } from "./money.js";
 const { EUR } = Currencies;
 
 const money2 = new Money("7", EUR());
-const money3 = new Money("6", EUR());
-const money4 = new Money("8", EUR());
+const money3 = new Money(6, EUR());
+const money4 = new Money(8.115, EUR());
 
 console.log(money2.gt(money3));
 console.log(money2.gte(money3));
